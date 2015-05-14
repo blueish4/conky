1e3a6c4da033fb14d1b19105fddda95782f5a297
Michael Hathaway <michael@hathaways.me.uk>
2015-05-14 20:49:54 +0100

    Split into two conkys, support for album art and initial amarok support

--- conky.spotify.nowplaying.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- 
-echo "  Artist:" `qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 Metadata | grep "artist" | cut -b 15-` 
-echo "  Title: " `qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 Metadata | grep "title" | cut -b 14-`
-echo "  Album: " `qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 Metadata | grep "album" | cut -b 14-`
